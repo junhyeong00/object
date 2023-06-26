e2a7616f0bca67331cd11d1cb8999f8f9ca0310b
junhyeong00 <jhbae0420@naver.com>
2023-06-26 20:47:19 +0900

    chapter01-step02

---
+++ chapter01/src/main/java/step02/TicketSeller.java
@@ -0,0 +1,17 @@
+package step02;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+
+        Long fee = audience.buy(ticket);
+
+        ticketOffice.plusAmount(fee);
+    }
+}
